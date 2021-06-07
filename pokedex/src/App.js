65163a39ae6320423a63604df609765759c2a29e
PSpirovskiEhmer <Pavel.SpirovskiEhmer-Extern@immowelt.de>
2021-06-07 12:15:35 +0200

    Starting pokemon search and display

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,30 +1,72 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import './App.css';
 import axios from "axios";
 
 const App = () => {
-  const [pokemon, setPokemon] = useState("Pikachu");
+  const [name, setPokemon] = useState("");
   const [pokemonData, setPokemonData] = useState([]);
-  const[pokemonType, setPokemonType] = useState("")
+  const [pokemonType, setPokemonType] = useState("")
+  const [pokemonIndex, setPokemonIndex] = useState("")
+  const [pokemonName, setPokemonName] = useState("")
 
   const getPokemon = async () => {
     const toArray = [];
     try {
-      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
-      const res = await axios.get(url)
-      console.log(res)
+      const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+      const res = await axios.get(url);
+      toArray.push(res.data);
+      setPokemonType(res.data.types[0].type.name)
+      setPokemonIndex(res.data.game_indices[0].game_index)
+      setPokemonName(res.data.species.name)
+      setPokemonData(toArray);
+      console.log(res);
     } catch (e) {
       console.log(e)
     }
   }
 
-  useEffect(() => {
-    getPokemon()
-  }, [])
+const handleChange = (e) => {
+  setPokemon(e.target.value.toLowerCase())
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  getPokemon();
+};
 
   return (
     <div className="app">
-      <h1>Torterra</h1>
+      <form onSubmit = {handleSubmit}>
+        <label>
+          <input
+            type = "text"
+            onChange = {handleChange}
+            placeholder = "Pokemon name: "
+          />
+        </label>
+      </form>
+      {pokemonData.map((data) => {
+        return(
+          <div className = "container-pokemon">
+            <img src={data.sprites["front_shiny"]} alt = "Couldn't load pokemon"/>
+            <div className = "divTable">
+              <div className = "divTableBody"></div>
+              <div className = "divTableRow">
+                <div className = "divTableCell">Name: </div>
+                <div key="{namepoke}" className = "divTableCell">{pokemonName}</div>
+              </div>
+              <div className = "divTableRow">
+                <div className = "divTableCell">Type: </div>
+                <div key="{typepoke}" className = "divTableCell">{pokemonType}</div>
+              </div>
+              <div className = "divTableRow">
+                <div className = "divTableCell">Number: </div>
+                <div key="{indexpoke}" className = "divTableCell">{pokemonIndex}</div>
+              </div>
+            </div>
+          </div>
+        )
+      })}
     </div>
   );
 }
