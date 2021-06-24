438b48c122dece7c8ad76f0c2e62f59f7e6680dc
PSpirovskiEhmer <Pavel.SpirovskiEhmer-Extern@immowelt.de>
2021-06-24 13:05:51 +0200

    type backgrounds and stats

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -301,6 +301,12 @@ const App = () => {
               name={pokemon.name}
               image={pokemon.sprites.front_shiny}
               type={pokemon.types[0].type.name}
+              hp={pokemon.stats[0].base_stat}
+              attack={pokemon.stats[1].base_stat}
+              defense={pokemon.stats[2].base_stat}
+              spatk={pokemon.stats[3].base_stat}
+              spdef={pokemon.stats[4].base_stat}
+              speed={pokemon.stats[5].base_stat}
               key={index}
             />)}
           <button className="load-more" onClick={() => getAllPokemon()}>Load Pokémon</button>
--- pokedex/src/components/PokemonDetails.js
+++ pokedex/src/components/PokemonDetails.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import './card.css';
 
-const PokemonDetails = ({ id, image, name, type }) => {
-    const style = type + " detailcontainer";
+const PokemonDetails = ({ id, image, name, type, hp, attack, defense, spatk, spdef, speed, total}) => {
+    const conteffect = type + " detailcontainer";
     return (
-        <div className={style}>
+        <div className={conteffect}>
             <img src={image} alt={name} />
             <div className="detail-wrapper">
                 <small>#0{id}</small>
                 <h5>{name}</h5>
                 <small>Type: {type}</small>
+                <table className="table">
+                    <tr>
+                        <td>HP: <br></br>{hp}</td>
+                        <td>Attack: {attack}</td>
+                        <td>Defense: {defense}</td>
+                    </tr>
+                    <tr>
+                        <td>Sp. Att: {spatk}</td>
+                        <td>Sp. Def: {spdef}</td>
+                        <td>Speed: <br></br>{speed}</td>
+                    </tr>
+                </table>
             </div>
         </div>
     )
